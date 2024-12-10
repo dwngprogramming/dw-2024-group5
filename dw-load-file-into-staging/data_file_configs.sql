870b6f8f1c17fa15623dd22822fdd518f7b97abd
Thái Tường An <161183479+thaituongan@users.noreply.github.com>
2024-12-10 11:30:03 +0700

    Update data_file_configs.sql

--- dw-load-file-into-staging/data_file_configs.sql
+++ dw-load-file-into-staging/data_file_configs.sql
@@ -1,41 +1,15 @@
-use control;
-DROP TABLE IF EXISTS data_file_configs;
-
--- Create table to store configuration settings for data files
-CREATE TABLE IF NOT EXISTS data_file_configs (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    description TEXT,
-    file_name VARCHAR(1000),       -- source file name
-    location VARCHAR(1000),          -- Location of the file (e.g., folder or system location)
-    format VARCHAR(255),             -- File format (e.g., "csv", "txt")
-    `separator` VARCHAR(255),        -- Field separator (e.g., ",", ";")
-    `columns` TEXT,                  -- Columns in the file (e.g., "product_name,image_url,...")
-    destination VARCHAR(1000),       -- Destination table (e.g., "staging.cp_daily")
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Creation timestamp
-    updated_at TIMESTAMP NULL,                      -- Update timestamp
-    created_by VARCHAR(255),                        -- User who created the entry
-    updated_by VARCHAR(255)                         -- User who last updated the entry
-);
-
--- Insert configuration settings into data_file_configs for the cp_daily CSV file
-INSERT INTO data_file_configs (
-    description,
-    file_name,
-    location,
-    format,
-    `separator`,
-    `columns`,
-    destination,
-    created_at,
-    created_by
-) VALUES (
-    'Configuration for loading cp_daily CSV data',
-    'cp_daily',
-    'C:/ProgramData/MySQL/MySQL Server 8.2/Uploads',
-    'csv',
-    ';',
-    'product_name, image_url, dimensions, weight, resolution, sensor, buttons, connection, battery, compatibility, utility, manufacturer, price',
-    'staging.cp_daily',
-    CURRENT_TIMESTAMP,
-    'admin'
-);
+CREATE TABLE `data_file_configs` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `file_name` varchar(1000) DEFAULT NULL,
+  `location` varchar(1000) DEFAULT NULL,
+  `format` varchar(255) DEFAULT NULL,
+  `separator` varchar(255) DEFAULT NULL,
+  `columns` text,
+  `destination` varchar(1000) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
